8ef67479b093d56eaee830a07fcbf0f1ff4bfa20
Zach Scott <zach.mobile@gmail.com>
2013-05-25 08:50:53 -0600

    Added docs to CurrencyPair

--- src/main/java/org/multibit/exchange/domainmodel/CurrencyPair.java
+++ src/main/java/org/multibit/exchange/domainmodel/CurrencyPair.java
@@ -3,15 +3,29 @@ package org.multibit.exchange.domainmodel;
 /**
  * <p>A ValueObject that provides the following to the domain model:</p>
  * <ul>
- * <li>A representation of a currency pair.</li>
+ * <li>Representation of a currency pair.</li>
  * </ul>
+ * <p/>
+ * A currency pair is the quotation of the relative value of a currency unit against the
+ * unit of another currency in a currency exchange market.
  *
  * @since 0.0.1
  *         
  */
 public class CurrencyPair {
 
+  /*
+   * The base currency is the currency that is quoted in relation to the counterCurrency. It is also
+   * referred to as the transaction currency.
+   *
+   * The term base currency is also used as the accounting currency by banks, and is usually the domestic currency.
+   */
   private final Currency baseCurrency;
+
+
+  /*
+   * The currency that is used as the reference is called the counter currency or quote currency.
+   */
   private final Currency counterCurrency;
 
   public CurrencyPair(Currency baseCurrency, Currency counterCurrency) {
